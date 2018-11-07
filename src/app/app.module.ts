479f7c1c5711cb9632b600ff6b13afa206e5e994
Rodolphe <dutchr@live.fr>
2018-11-07 12:26:47 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {Router, RouterModule} from '@angular/router';
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {AngularFireModule} from 'angularfire2';
